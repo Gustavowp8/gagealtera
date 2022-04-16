ede6d0381b01ab746fbbefdf0320757002f27607
Gustavo Ferreira <gustavoferreira73@live.com>
2022-04-16 12:40:01 -0300

    atualização

---
+++ funcao/app.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+  });
+
+  function fechaMenu(){
+    menu.style.display='none'
+  }
+
+  function chamaMenu(){
+    menu.style.display='block'
+  }
